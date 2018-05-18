756db256fc98ed5e3410fb7e3f3cc719f2484497
BillyZD <13036151491@163.com>
2018-05-18 20:24:28 +0800

    Update ZDIcaouselView.swift
    ddddd

--- ZDIcaousel/view/ZDIcaouselView.swift
+++ ZDIcaousel/view/ZDIcaouselView.swift
@@ -41,9 +41,6 @@ class ZDIcaouselView: UIView{
         super.init(frame: frame)
         self.backgroundColor = UIColor.black
     }
-    ////
-    ////
-    ///
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
