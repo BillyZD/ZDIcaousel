c9481805eab9bb42b27acf3ed6c863dee0950f87
admin <admin@BillydeiMac.local>
2018-05-22 18:51:01 +0800

    test

--- ZDIcaousel/base/AppDelegate.swift
+++ ZDIcaousel/base/AppDelegate.swift
@@ -13,6 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    //// addddddd
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
