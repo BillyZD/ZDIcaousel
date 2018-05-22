18e0bde70bd7a2d8de00a4be2f48acc9306e8133
admin <admin@BillydeiMac.local>
2018-05-22 19:17:51 +0800

    111

--- ZDIcaousel/base/AppDelegate.swift
+++ ZDIcaousel/base/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     //// addddddd
     ///bbbbbbbb
+    ///aaa
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
